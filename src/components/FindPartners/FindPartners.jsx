3d86c9a6a32de6750457d6591db6ac3e206b60a8
salauddin kamran <kamransalauddin186@gmail.com>
2025-11-17 10:52:39 +0600

    search added

--- src/components/FindPartners/FindPartners.jsx
+++ src/components/FindPartners/FindPartners.jsx
@@ -1,4 +1,4 @@
-import React, { use } from "react";
+import React, { use, useState } from "react";
 import { useLoaderData } from "react-router";
 import FindPartnerCard from "../FindPartnerCard/FindPartnerCard";
 import MyContainer from "../MyContainer/MyContainer";
@@ -7,20 +7,48 @@ import { PuffLoader } from "react-spinners";
 
 const FindPartners = () => {
   const datas = useLoaderData();
-  const { loading } = use(AuthContext);
+  // const { loading } = use(AuthContext);
+  const [partners, setPartners] = useState(datas);
+  const [loading, setLoading] = useState(false)
+
+  const handleSearch = (e)=> {
+    e.preventDefault()
+    const search_text = e.target.search.value;
+    setLoading(true)
+    // console.log(search_text)
+    fetch(`http://localhost:3000/search?search=${search_text}`)
+    .then(res => res.json())
+    .then(data => {
+     setPartners(data)
+     setLoading(false)
+    })
+  }
   return (
     <MyContainer>
       <div>
         <h1 className="text-5xl font-bold text-red-400 text-center my-10">
           Find Your Study Patrners
         </h1>
+        <form onSubmit={handleSearch} className="flex justify-end mb-10">
+          <div className="join">
+            <div>
+              <div>
+                <input name="search" className="input join-item" placeholder="Search" />
+              </div>
+            </div>
+            <div className="indicator">
+              <button className="btn join-item">Search</button>
+            </div>
+          </div>
+        </form>
+
         {loading ? (
           <div className="w-[200px] h-screen mx-auto flex items-center justify-center">
             <PuffLoader />
           </div>
         ) : (
-          <div className="grid gap-10 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 my-28">
-            {datas.map((fidnData) => (
+          <div className="grid gap-10 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mb-28">
+            {partners.map((fidnData) => (
               <FindPartnerCard
                 key={fidnData._id}
                 fidnData={fidnData}
